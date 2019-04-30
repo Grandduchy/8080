22c3dfaea6957aacd727ee555f7f32a2a8d9968b
Grandduchy <jchallenger1@hotmail.com>
2019-04-30 16:36:50 -0400

    Added few new opcodes

--- src/Disassembler8080.cpp
+++ src/Disassembler8080.cpp
@@ -11,8 +11,8 @@
 #define EXECOPCODE(obj, ptr) ((obj).*(ptr))() // executes a pointer to a function member with the object
 #define SETZERO(expr) (state.condFlags.zero = (((expr) & 0xff) == 0) ? 1 : 0)
 #define SETSIGN(expr) (state.condFlags.sign = (((expr) & 0x80) != 0) ? 1 : 0 )
-#define SETCARRY(expr) (state.condFlags.carry = (expr) > 0xff)
-// count all bits, if even set flag to 1 if odd set to 0
+#define SETCARRY(expr, maxVal) (state.condFlags.carry = (expr) > (maxVal))
+// count all bits, if sum is even set flag to 1 if odd set to 0
 // use gnu's optimised function if possible
 #ifdef __GNUC__
     #define SETPARITY(expr) (state.condFlags.parity = !((__builtin_popcount(expr) & 0x1) == 1))
@@ -29,6 +29,8 @@ Disassembler8080::Disassembler8080() {
     opcodeTable[0x01] = &Disassembler8080::OP_LXIB_D16;
     opcodeTable[0x05] = &Disassembler8080::OP_DCRB;
     opcodeTable[0x06] = &Disassembler8080::OP_MVIB_D8;
+    opcodeTable[0x09] = &Disassembler8080::OP_DADB;
+    opcodeTable[0x0d] = &Disassembler8080::OP_DCRC;
 }
 
 
@@ -89,6 +91,24 @@ void Disassembler8080::OP_MVIB_D8() {
     ++state.programCounter;
 }
 
+// Register pair B & C is added to H & L register pair
+void Disassembler8080::OP_DADB() {
+    // combine B & C and H & L
+    uint16_t BC = static_cast<uint16_t>( (static_cast<uint16_t>(state.b) << 8) | state.c);
+    uint16_t HL = static_cast<uint16_t>( (static_cast<uint16_t>(state.h) << 8) | state.l);
+    uint32_t sum = BC + HL;
+    SETCARRY(sum, 0xFFFF);
+    sum &= 0xFFFF;
+    // set H & L to the new sum.
+    state.l = sum & 0x00FF;
+    state.h = (sum & 0xFF00) >> 8;
+}
 
+void Disassembler8080::OP_DCRC() {
+    --state.c;
+    SETZERO(state.c);
+    SETSIGN(state.c);
+    SETPARITY(state.c);
+}
 
 
