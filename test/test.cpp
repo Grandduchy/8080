d5eada05c0e2f5c5ee9cd1c8e9aa80eaa599c5a1
Grandduchy <jchallenger1@hotmail.com>
2019-05-02 15:36:14 -0400

    Added a test

--- test/test.cpp
+++ test/test.cpp
@@ -3,19 +3,51 @@
 #include "State8080.hpp"
 #include "Disassembler8080.hpp"
 
+std::string lineNum() {
+    return std::to_string(__LINE__);
+}
+
 BOOST_AUTO_TEST_CASE( opcode_tests ) {
     State8080 state;
     std::array<uint8_t, State8080::RAM>& memory = state.memory;
-
-    // OP_LXIB_D16 test using 0x01
-    memory[0] = 0x01;
-    memory[1] = 0x50;
-    memory[2] = 0x80;
     Disassembler8080 dis;
-    dis.runCycle(state);
-    if (!(state.b == 0x80 && state.c == 0x50)) {
-        std::string a("0x01 OP LIXB->D16 failure in b=" + std::to_string(state.b) + ", c=" + std::to_string(state.c));
-        BOOST_ERROR(a);
+
+    {// OP_LXIB_D16 test using 0x01, if this test succeeds then it is safe to assume LXID_D16 and other
+        // instructions are also safe as it uses the same code.
+
+        memory[0] = 0x01;
+        memory[1] = 0x50;
+        memory[2] = 0x80;
+        dis.runCycle(state);
+        if (!(state.b == 0x80 && state.c == 0x50)) {
+            std::string s("0x01 OP LIXB->D16 failure in b=" + std::to_string(state.b) + ", c=" + std::to_string(state.c));
+            BOOST_ERROR(s);
+        }
     }
+    { // OP_DCRB 0x05
+        state.clearSpecial();
+        memory[0] = 0x05;
+        // check if it decriments a number
+        state.b = 0xF;
+        dis.runCycle(state);
+        if (state.b != 0xE){
+            std::string s("0x05 OP_DCRB failure, b=" + std::to_string(state.b));
+            BOOST_ERROR(s);
+        }
+        state.clearSpecial();
+        // check if zero is set
+        state.b = 0x1;
+        dis.runCycle(state);
+        if (state.condFlags.zero == 0)
+            BOOST_ERROR("0x05 OP_DCRB failure, zero not set");
+        state.clearSpecial();
+        // check if sign is affected
+        state.b = 0xFF;
+        dis.runCycle(state);
+        if (state.condFlags.sign == 1)
+            BOOST_ERROR("0x05 OP_DCRB failure, sign not set");
+
+    }
+
 
 }
