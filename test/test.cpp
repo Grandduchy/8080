7f1f8195b027c4a20d8e8c824c573fa586cafeb3
Grandduchy <jchallenger1@hotmail.com>
2019-05-02 15:58:18 -0400

    Added more tests

--- test/test.cpp
+++ test/test.cpp
@@ -3,10 +3,6 @@
 #include "State8080.hpp"
 #include "Disassembler8080.hpp"
 
-std::string lineNum() {
-    return std::to_string(__LINE__);
-}
-
 BOOST_AUTO_TEST_CASE( opcode_tests ) {
     State8080 state;
     std::array<uint8_t, State8080::RAM>& memory = state.memory;
@@ -24,7 +20,7 @@ BOOST_AUTO_TEST_CASE( opcode_tests ) {
             BOOST_ERROR(s);
         }
     }
-    { // OP_DCRB 0x05
+    { // OP_DCR B 0x05
         state.clearSpecial();
         memory[0] = 0x05;
         // check if it decriments a number
@@ -44,10 +40,29 @@ BOOST_AUTO_TEST_CASE( opcode_tests ) {
         // check if sign is affected
         state.b = 0xFF;
         dis.runCycle(state);
-        if (state.condFlags.sign == 1)
+        if (state.condFlags.sign == 0)
             BOOST_ERROR("0x05 OP_DCRB failure, sign not set");
 
     }
-
+    { // OP_MVIB_D8 0x06
+        state.clearAll();
+        memory[0] = 0x06;
+        memory[1] = 0xF8;
+        dis.runCycle(state);
+        if (state.b != 0xF8)
+            BOOST_ERROR("0x06 OP_MVIB_D8 failure, b=" + std::to_string(state.b));
+    }
+    {// OP_DAD B // 0x09
+        state.clearAll();
+        memory[0] = 0x09;
+        state.b = 0x33;
+        state.c = 0x9F;
+        state.h = 0xA1;
+        state.l = 0x7B;
+        dis.runCycle(state);
+        bool passed = state.h == 0xD5 && state.l == 0x1A && state.condFlags.carry == 0;
+        if (!passed)
+            BOOST_ERROR("0x09 OP_DAD B failure");
+    }
 
 }
