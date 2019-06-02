016a0c35ae1431571c1bdebe5c6b75b696b82d79
Grandduchy <jchallenger1@hotmail.com>
2019-06-02 14:18:26 -0400

    comments

--- include/State8080.hpp
+++ include/State8080.hpp
@@ -20,14 +20,16 @@ struct State8080 {
     uint8_t h;
     uint8_t l;
     uint8_t a; //accumulator register
+
     static constexpr uint16_t RAM = 0xFFFF;
     std::array<uint8_t, RAM> memory;
     uint32_t cycleCount = 0;
-    // Variables for shift registering
+    // Variables for shift registering, both LHS and RHS create a uint16_t
     uint8_t shiftLHS = 0;
     uint8_t shiftRHS = 0;
-    int shiftOffset = 0;
+    int shiftOffset = 0; // offset to shift from [0, 15] but starts from reverse direction
 
+    // Ports for I/O instructions
     uint8_t port1 = 0;
     uint8_t port2 = 0;
     uint8_t port3 = 0;
